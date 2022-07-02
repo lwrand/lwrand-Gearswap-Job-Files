49a14abcda3239af72799553168ab96d319146ec
lwrand <lwrand@users.noreply.github.com>
2022-07-02 20:47:11 +0200

    Delete Croex directory

--- Croex/User-Globals.lua
+++
@@ -1,8 +0,0 @@
-function set_lockstyle_custom(styleid)
-	add_to_chat(217, 'Style '..styleid..' is ON!')
-    send_command('wait 4; input /lockstyleset ' .. styleid)
-end
-
-sets.precast.FC.Trust =  {
-	body="Yoran Unity Shirt"
-}
